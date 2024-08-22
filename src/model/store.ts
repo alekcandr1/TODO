7c93a10890d2652ce209fc82a3935b448b773b8e
alekcandr1 <alekcandrmain@gmail.com>
2024-08-22 13:13:07 +0300

    add RTK

--- src/model/store.ts
+++ src/model/store.ts
@@ -1,10 +1,11 @@
-import { AnyAction, applyMiddleware, combineReducers, legacy_createStore } from "redux"
+import { combineReducers } from "redux"
 import { TasksActionsType, tasksReducer } from "./task-reducer"
 import { TodolistsActionsType, todolistsReducer } from "./todolists-reducer"
-import { thunk, ThunkAction, ThunkDispatch } from "redux-thunk"
+import { ThunkAction, ThunkDispatch } from "redux-thunk"
 import { useDispatch } from "react-redux"
 import { appReducer } from "./app-reducer"
 import { authReducer } from "./auth-reducer"
+import { configureStore, UnknownAction } from "@reduxjs/toolkit"
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -14,16 +15,19 @@ const rootReducer = combineReducers({
   todolists: todolistsReducer,
   auth: authReducer,
 })
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+// export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+
+export const store = configureStore({ reducer: rootReducer })
 
 // определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
+export type AppRootStateType = ReturnType<typeof store.getState>
 
-export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
-export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
+export type AppDispatch = ThunkDispatch<AppRootStateType, any, UnknownAction>
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, UnknownAction>
 
 export type AppThunkType = TodolistsActionsType | TasksActionsType
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppThunkType>
 
 // @ts-ignore
 window.store = store
